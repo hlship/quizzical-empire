7845e62d0b70096e7c253c38328d56af8e34aa32
Merlyn Albery-Speyer <curious.attempt.bunny@gmail.com>
2011-10-10 21:15:26 -0700

    experimenting with express

--- web.js
+++ web.js
@@ -1,5 +1,9 @@
-var http = require('http');
-http.createServer(function (req, res) {
+var app = require('express').createServer();
+
+app.get('/', function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end("<html><body><h1>Hello from NodeJS</h1></body></html>");
-}).listen(8080, "127.0.0.1");
+  res.send('hello world');
+});
+
+app.listen(8080, "127.0.0.1");
