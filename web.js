a6e13393ea0f0a6695a2bb4b8ad3d065d8e7cc63
Howard M. Lewis Ship <hlship@gmail.com>
2011-10-10 21:54:04 -0700

    Convert the web app to display the contents of the "ls -laR"

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
 
+var exec = require('child_process').exec;
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+  console.log("Received request for /");
+
+  exec("ls -laR", function (error, stdout, stdin) {
+      response.writeHeader(200, { "Content-Type": "text/plain" });
+      response.write(stdout);
+      response.end();
+  });
+
 });
 
 var port = process.env.PORT || 8080;
